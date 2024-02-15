c9abf7b872735021aba79743af2f0de4b9eda29c
Santosh Sigdel <santosh.sigdel900@gmail.com>
2024-02-16 00:22:18 +0200

    Done

---
+++ Onlineodering/header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bagmati Restaurant</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style4.css">
+</head>
+<body>
+
+<header>
+    <div class="logo-container">
+        <a href="index.php">
+            <img src="images/BR.png" alt="Restaurant Logo" width="50" height="50">
+        </a>
+        <div class="restaurant-info">
+            <h1>Bagmati Restaurant</h1>
+            <p class="smaller-text">Breakfast and lunch served every day</p>
+        </div>
+    </div>
+    <nav>
+        <a href="index.php">Home</a>
+        <a href="menu.php">Menu</a>
+        <a href="contact.php">Contact</a>
+        <a href="cart.php">Cart</a>
+        <a href="careers.php">Careers</a>
+    </nav>
+</header>
+
+    <section id="home">
