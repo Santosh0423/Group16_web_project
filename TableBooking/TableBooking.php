a036a667161e22dff7bdc37d58c55d8bdab15bb3
BibekPandey7 <146955877+BibekPandey7@users.noreply.github.com>
2024-03-14 22:33:52 +0200

    done

--- TableBooking/TableBooking.php
+++ TableBooking/TableBooking.php
@@ -5,10 +5,10 @@ include 'header.php'
 <h1> Online Table Booking </h1>
 <form method="post" name="table_booking_form" action="process.php">
     
-    First Name: <input type="text" name="first_name"> <br><br>
-    Last Name: <input type="text" name="last_name"> <br><br>
-    Email: <input type="email" name="email"> <br><br>
-    Phone Number: <input type="text" name="phone_number"> <br><br>
+    First Name: <input type="text" name="first_name" required> <br><br>
+    Last Name: <input type="text" name="last_name" required> <br><br>
+    Email: <input type="email" name="email" required> <br><br>
+    Phone Number: <input type="text" name="phone_number" required> <br><br>
     Reservation Date: <input type="date" name="reservation_date" required> <br><br>
     Reservation Time: <input type="time" name="reservation_time" required> <br><br>
     Table Number: <input type="number" name="table_number" min="1" max="10" required > <br><br>
@@ -21,14 +21,18 @@ include 'header.php'
         <option value="business">Business Meeting</option>
     </select><br><br>
     
-    Notes : <input type="text" name="notes" maxlength="20" required > <br><br>
-
-
+    
+    
     <button type="submit" class="btn btn-primary" name="submit">Submit</button>
 </form>
 <?php
 include 'footer.php'
 ?>
+
+
+
+
+
 
 
 
